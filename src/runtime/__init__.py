8b59993e018a1c6f09a2a03b2c9d40c46cce5a6f
Israel Oluwajuwon Adewuyi <i.adewuyi@innopolis.university>
2026-05-05 00:18:13 +0300

    added runtime configs, copied other fns from ddp for pp

--- src/runtime/__init__.py
+++ src/runtime/__init__.py
@@ -1,5 +1,6 @@
 from src.runtime.ddp import DDPRuntime
 from src.runtime.ddp_reference import DDPRuntimeRef
+from src.runtime.naive_pp import NaivePipelineParallel
 from src.runtime.single import SingleDeviceRuntime
 
-__all__ = ["DDPRuntime", "DDPRuntimeRef", "SingleDeviceRuntime"]
+__all__ = ["DDPRuntime", "DDPRuntimeRef", "SingleDeviceRuntime", "NaivePipelineParallel"]
