24a33d81b0a18174d2525b8c10405fddf4e3b60f
Israel Adewuyi <isistickz@gmail.com>
2026-06-06 00:23:11 +0000

    fixes

--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -31,7 +31,7 @@ int main(){
 
         std::vector<float>ones_arr(count), twos_arr(count), out_arr(count);
 
-        for(int i = 0; i < count; i++){
+        for(size_t i = 0; i < count; i++){
             ones_arr[i] = 1.0;
             twos_arr[i] = 2.0;
         }
