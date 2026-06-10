40038c5878cb884a8629924a1580ca0395d1651d
Israel Oluwajuwon Adewuyi <i.adewuyi@innopolis.university>
2026-06-10 03:12:41 +0300

    record traces for certain parts of MoE

--- src/model/feed_fwd.py
+++ src/model/feed_fwd.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from jaxtyping import Float
+from torch.profiler import record_function
 
 from src.config import ModelConfig
 
@@ -46,9 +47,12 @@ class MoE(nn.Module):
         flat_tokens = x.reshape(-1, d_model)  # (num_tokens d_model)
 
         # get the expert logits
-        expert_logits = self.router(flat_tokens)
-        expert_probs = expert_logits.softmax(dim=-1)
-        topk_weights, topk_expert_idx = torch.topk(expert_probs, dim=-1, k=self.cfg.top_k)
+        with record_function("moe/router"):
+            expert_logits = self.router(flat_tokens)
+
+        with record_function("moe/topK"):
+            expert_probs = expert_logits.softmax(dim=-1)
+            topk_weights, topk_expert_idx = torch.topk(expert_probs, dim=-1, k=self.cfg.top_k)
         expert_weights = topk_weights / topk_weights.sum(dim=-1, keepdim=True)
 
         # accumulate the weighted results of each expert per token
@@ -56,17 +60,19 @@ class MoE(nn.Module):
         tokens_per_expert = torch.zeros((self.cfg.num_experts), dtype=torch.long, device=x.device)
 
         for expert in range(self.cfg.num_experts):
-            # Return a 2D tensor of which token chose the current expert and the index / position in the topK
-            indices = torch.argwhere(topk_expert_idx == expert)
-            # if no token chose current expert, continue
-            if indices.numel() == 0:
-                continue
-            # get the residual stream vector for each token by indexing into flat tokens with all tokens that chose current expert
-            expert_toks = flat_tokens[indices[:, 0]]
+            with record_function("moe/dispatch"):
+                # Return a 2D tensor of which token chose the current expert and the index / position in the topK
+                indices = torch.argwhere(topk_expert_idx == expert)
+                # if no token chose current expert, continue
+                if indices.numel() == 0:
+                    continue
+                # get the residual stream vector for each token by indexing into flat tokens with all tokens that chose current expert
+                expert_toks = flat_tokens[indices[:, 0]]
             tokens_per_expert[expert] += expert_toks.shape[0]
 
-            pool[indices[:, 0]] += self.experts[expert](expert_toks) * expert_weights[
-                indices[:, 0], indices[:, 1]
-            ].unsqueeze(1)
+            with record_function("moe/expert_combine"):
+                pool[indices[:, 0]] += self.experts[expert](expert_toks) * expert_weights[
+                    indices[:, 0], indices[:, 1]
+                ].unsqueeze(1)
 
         return (pool.reshape(batch, seq_len, d_model), tokens_per_expert)
